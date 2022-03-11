9ad8728910adfd6f4a9d1401f9ef129d3a61c846
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-03-11 14:47:09 +0200

    datingDB'

--- DatingDB/DatingDB.sql
+++ DatingDB/DatingDB.sql
@@ -4,43 +4,44 @@ CREATE TABLE my_contacts (
     contact_id bigserial CONSTRAINT contact_id_key PRIMARY KEY,
     last_name varchar(20) NOT NULL,
     first_name varchar(100) NOT NULL,
-    phone integer(10) NOT NULL,
+    phone integer NOT NULL,
     email varchar(50) NOT NULL constraint email_constraint unique,
     gender varchar(50) NOT NULL, 
     birthday integer NOT NULL,
     prof_id integer NOT NULL references profession (prof_id),
-    zip_code integer NOT NULL references zip_code (zip_code),
+    zip_code varchar(4) NOT NULL references zip_code (zip_code),
     status_id integer NOT NULL references status (status_id)
 );
 
 CREATE TABLE profession (
     prof_id bigserial CONSTRAINT prof_id_key PRIMARY KEY,
-    profession varchar(25) constraint profession_key unique
+    profession varchar(25) constraint profession_key unique);
 
 CREATE TABLE zip_code (
     zip_code varchar(4)  CONSTRAINT zip_code_key PRIMARY KEY,
     city varchar(25) NOT NULL,
-    province varchar(25) NOT NULL,
+    province varchar(25) NOT NULL);
 
 CREATE TABLE status (
     status_id bigserial CONSTRAINT status_id_key PRIMARY KEY,
-    status varchar(25) NOT NULL;
+    status varchar(25) NOT NULL);
 
-CREATE TABLE contact_intrest (
+CREATE TABLE contact_interest (
     contact_id bigserial references my_contacts(contact_id),
-    intrest_id varchar(25) references interests (interest_id);
+    interest_id integer references interests (interest_id));
 
 CREATE TABLE contact_seeking (
     contact_id bigserial references my_contacts (contact_id),
-    seeking_id varchar(25) references seeking (seeking_id);
+    seeking_id integer references seeking (seeking_id));
 
-CREATE TABLE intrests (
-    intrest_id bigserial CONSTRAINT intrest_id_key PRIMARY KEY,
-    intrest varchar(25) NOT NULL;
+CREATE TABLE interests (
+    interest_id bigserial CONSTRAINT intrest_id_key PRIMARY KEY,
+    interest varchar(25) NOT NULL);
+drop table interests;	
 
 CREATE TABLE seeking (
     seeking_id bigserial CONSTRAINT seeking_id_key PRIMARY KEY,
-    seeking varchar(25) NOT NULL;
+    seeking varchar(25) NOT NULL);
 
 --Populating The Tables-- 
 
